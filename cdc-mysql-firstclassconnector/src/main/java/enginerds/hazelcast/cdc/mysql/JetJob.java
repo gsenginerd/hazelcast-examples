905b38fcfb305b2cad0240ca26a6443702bf07c9
Subramaniam G <sgopalakrishnan@hl.com>
2022-11-22 12:00:23 +0400

    Version upgrade

--- cdc-mysql-firstclassconnector/src/main/java/enginerds/hazelcast/cdc/mysql/JetJob.java
+++ cdc-mysql-firstclassconnector/src/main/java/enginerds/hazelcast/cdc/mysql/JetJob.java
@@ -22,22 +22,22 @@ public class JetJob {
 
 	public static void main(String[] args) {
 		StreamSource<ChangeRecord> source = MySqlCdcSources.mysql("people").setDatabaseAddress("host.docker.internal")
-				.setDatabasePort(3306).setDatabaseUser("root").setDatabasePassword("P@$$w0rd1")
-				.setClusterName("conflicts-check-dev").setDatabaseWhitelist("cc_hazelcast")
-				.setTableWhitelist("cc_hazelcast.people").build();
+				.setDatabasePort(3306).setDatabaseUser("cdcuser").setDatabasePassword("P@$$w0rd1").setClusterName("dev")
+				.setDatabaseWhitelist("cc_hazelcast").setTableWhitelist("cc_hazelcast.people")
+				.build();
 
 		Pipeline pipeline = Pipeline.create();
-		pipeline.readFrom(source).withoutTimestamps().peek()
-				.writeTo(CdcSinks.map("people", r -> r.key().toMap().get("id"), r -> r.value().toObject(People.class).name));
+		pipeline.readFrom(source).withoutTimestamps().peek().writeTo(
+				CdcSinks.map("people", r -> r.key().toMap().get("id"), r -> r.value().toObject(People.class).name));
 
 		JobConfig cfg = new JobConfig().setName("mysql-people-monitor");
 		HazelcastInstance hz = Hazelcast.bootstrappedInstance();
-		
+
 		// Delete map if exists
 		IMap<Integer, Object> peopleMap = hz.getMap("people");
 		if (peopleMap != null)
 			peopleMap.destroy();
-		
+
 		hz.getJet().newJob(pipeline, cfg);
 	}
 
